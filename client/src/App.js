57c88f55f9df954ccd10fa100b70ec33310313f5
sourcemasters <yognautdave@outlook.com>
2019-02-13 17:11:40 -0800

    express backend now connected to basic react homepage

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    data: null,
+  };
+
+  componentDidMount()
+  {
+    // once the component mounts, call the fetch function below
+    this.callBackendAPI()
+      .then(res => this.setState({data: res.express}))
+      .catch(err => console.log(err));
+  }
+  
+  // Fetch GET route from Express server
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status != 200) throw Error(body.message);
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Random Species Generator</h1>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+
+        <p className="App-intro">{this.state.data}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// indicate that server is up and running on console.log
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// GET route as placeholder to check backend connection
+app.get('/express_backend', (req, res) => {
+    res.send({express: 'express backend now connected to react'});
+});
